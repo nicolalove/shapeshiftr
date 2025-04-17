4a9e0880d40907f4be6c58adcab5d899984da8ba
Nicola Love <nicolalove@Nicolas-MacBook-Air.local>
2025-04-17 00:12:56 -0700

    changed iid and cvmetrics_sf

--- R/cvmetrics_sf.R
+++ R/cvmetrics_sf.R
@@ -1,14 +1,33 @@
-cvmetrics <- function(iid_dataset, grp_by = NULL, iidist_column = NULL, log10 = FALSE){
+cvmetrics <- function(iid_dataset, grp_by = NULL, iidist_col = NULL, idcol = NULL, log10 = FALSE){
     if (is.null(grp_by) || length(grp_by) == 0) {
       stop("Please specify one or more columns to group the inter-individual distances by, e.g., cvmetrics(dataset, grp_by = c('hour', 'yday'))")
     }
-  if (is.null(iidist_column) || length(iidist_column) == 0) {
-    stop("Please specify the column that contains the inter-individual distances by whatever time-unit you specified in the `grp_by` argument. If you used iidist() to calculate this, the column name will be `iidist`. e.g.,cvmetrics(dataset, grp_by = c('hour', 'yday'), iidist_column = iidist)")
+  if (is.null(iidist_col) || length(iidist_col) == 0) {
+    stop("Please specify the column that contains the inter-individual distances by whatever time-unit you specified in the `grp_by` argument. If you used iidist() to calculate this, the column name will be `iidist`. e.g.,cvmetrics(dataset, grp_by = c('hour', 'yday'), iidist_col = iidist)")
+  }
+  if (is.null(idcol) || length(idcol) == 0) {
+    stop("Please specify the column that contains the names/IDs of the individuals, e.g., cvmetrics(dataset, grp_by = c('hour', 'yday'), iidist_col = AID)")
   }
   if (log10) {
-    popcv <- iid_dataset %>% group_by(all_of(grp_by)) %>% summarise(cvpop = log10(sd({{iidist_column}}, na.rm = T)/mean({{iidist_column}}, na.rm = T)))
+    popcv <- iid_dataset %>% group_by(all_of(grp_by)) %>%
+        summarise(cvpop = log10(sd({{iidist_col}}, na.rm = T)/mean({{iidist_col}}, na.rm = T)))
+    indivcv <- iid_dataset %>% group_by(idcol, all_of(grp_by)) %>%
+      summarise(meanii = mean({{iidist_col}}, na.rm = T)) %>% ungroup() %>%
+      group_by(all_of(grp_by)) %>%
+      summarise(cvind = if_else(mean(meanii, na.rm = T) == 0, 0,
+                                log10(sd(meanii,na.rm = T)/mean(meanii, na.rm = T))))
+    total <- left_join(popcv, indivcv)
+    total$ratio <- total$indivcv - total$popcv
   } else {
-    popcv <- iid_dataset %>% group_by(all_of(grp_by)) %>% summarise(cvpop = sd({{iidist_column}}, na.rm = T)/mean({{iidist_column}}, na.rm = T))
+    popcv <- iid_dataset %>% group_by(all_of(grp_by)) %>%
+        summarise(cvpop = sd({{iidist_col}}, na.rm = T)/mean({{iidist_col}}, na.rm = T))
+    indivcv <- iid_dataset %>% group_by(idcol, all_of(grp_by)) %>%
+      summarise(meanii = mean({{iidist_col}}, na.rm = T)) %>% ungroup() %>%
+      group_by(all_of(grp_by)) %>%
+      summarise(cvind = if_else(mean(meanii, na.rm = T) == 0, 0,
+                                sd(meanii,na.rm = T)/mean(meanii, na.rm = T)))
+    total <- left_join(popcv, indivcv)
+    total$ratio <- total$indivcv / total$popcv
   }
 
 
--- R/iidist.R
+++ R/iidist.R
@@ -1,6 +1,9 @@
-iidist <- function(dataset, nest_by = NULL, ...){
+iidist <- function(dataset, nest_by = NULL, idcol = NULL, ...){
   if (is.null(nest_by) || length(nest_by) == 0) {
     stop("Please specify one or more columns to nest by, e.g., iidist(dataset, nest_by = c('year', 'season'))")
+  }
+  if (is.null(idcol) || length(idcol) == 0) {
+    stop("Please specify the column that contains the names/IDs of the individuals")
   }
   if (st_is_sf(dataset) == FALSE) {
     stop("Please convert your dataframe into an sf object using the function st_as_sf(dataframe, coords = c(X, Y), crs = #) from the sf package")
@@ -10,26 +13,26 @@ iidist <- function(dataset, nest_by = NULL, ...){
     nest(data = -dplyr::all_of(nest_by)) %>%
     mutate(
       pairdist = map(data, ~{
+        df <- .x
+        if (nrow(df) < 2) {
+          return(tibble(ID1 = NA, ID2 = NA, iidist = NA))
+        }
         tryCatch({
-          dist_matrix <- st_distance(.)
-          n <- nrow(dist_matrix)
-          if (is.null(n) || n == 0) {
-            return(data.frame(ID1 = NA, ID2 = NA, iidist = NA))
-          }
-          combs <- expand.grid(ID1 = 1:n, ID2 = 1:n) %>%
-            filter(ID1 < ID2) %>%
-            mutate(iidist = dist_matrix[cbind(ID1, ID2)])
-          # Add original IDs
-          combs$ID1 <- rownames(.)[combs$ID1]
-          combs$ID2 <- rownames(.)[combs$ID2]
+          dist_matrix <- st_distance(df)
+          ids <- df${{idcol}}
+          combs <- expand.grid(ID1 = ids, ID2 = ids) %>% filter(ID1 != ID2)
+
+          idx1 <- match(combs$ID1, ids)
+          idx2 <- match(combs$ID2, ids)
+
+          combs$iidist <- dist_matrix[cbind(idx1, idx2)]
           return(combs)
         }, error = function(e) {
-          # Return NA in case of error
-          return(data.frame(ID1 = NA, ID2 = NA, iidist = NA))
+          return(tibble(ID1 = NA, ID2 = NA, iidist = NA))
         })
       })
     ) %>%
-    select(-data) %>%
+    dplyr::select(-data) %>%
     unnest(pairdist) %>%
     distinct()
 }
